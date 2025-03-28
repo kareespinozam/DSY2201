921ef24d2dd0e7adcaab90bbd795cc9a60e5cbfa
Karel Espinoza <Kespinoza@ALSACIAREPUESTOS.CL>
2025-03-28 10:03:37 -0300

    Primer comit - Proyecto semana 2

---
+++ src/test/java/com/semana2/semana2/Semana2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semana2.semana2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
